271e7fcb41288671baf234212ef2f04150afcf8f
PavloShutz <pavlo050706@gmail.com>
2023-01-05 12:20:53 +0200

    added setup tool. changed project name

--- book_writer/__init__.py
+++ book_writer/__init__.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="Hop4key2un-",
-        DATABASE=os.path.join(app.instance_path, 'library.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'book_writer.sqlite'),
     )
 
     if test_config is None:
