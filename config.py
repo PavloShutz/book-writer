7153846a9766ae52b32dd7467eff854ae427646c
PavloShutz <pavlo050706@gmail.com>
2023-01-05 16:58:34 +0200

    small changes

--- config.py
+++ config.py
@@ -8,4 +8,3 @@ class Config(object):
     DEBUG = False
     SECRET_KEY = "YOUR SECRET KEY HERE"
     DATABASE = os.path.join(current_app.instance_path, 'book_writer.sqlite')
-Gs1goHDCHPVqng3zdZwl6w
