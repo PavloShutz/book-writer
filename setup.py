f2f185cc26f17cfc9852412eeb1826749209da96
PavloShutz <pavlo050706@gmail.com>
2023-01-21 11:27:58 +0200

    changed setup tools

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='book_writer',
-    version='0.0.1',
+    version='0.0.2-alpha',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
+        'bootstrap-flask',
+        'flask-paginate',
+        'flask-wtf',
+        'email_validator'
     ],
 )
